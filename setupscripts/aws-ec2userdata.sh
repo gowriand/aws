977411a119a074631810288ce23610f82ef48c1c
gowriand <95476026+gowriand@users.noreply.github.com>
2022-02-24 21:21:57 +0530

    Update aws-ec2userdata.sh

--- setupscripts/aws-ec2userdata.sh
+++ setupscripts/aws-ec2userdata.sh
@@ -1,8 +1,17 @@
-
 #!/bin/bash
+###########################
+# Sample- Give app separated by space 
+## Add puppet last only as it is followed by a reboot
+# applist=(jenkins jenkinslave awscli docker puppet)
+####################################
 
-sudo apt-get update
-sudo apt install -y apache2
-sudo systemctl start apache2
-cp /var/www/html/index.html /var/www/html/index.html_bak
-echo "`hostname`" > /var/www/html/index.html
+applist=(jenkinslave docker awscli)
+apt install -y net-tools
+for app in ${applist[@]}
+do
+    echo $app
+    cd /
+    sudo git clone https://github.com/gowriand/$app.git
+    cd /$app/setupscripts/
+    sudo sh -x setup-$app.sh
+done
